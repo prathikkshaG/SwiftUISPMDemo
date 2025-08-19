a6f8076ee8b93b06b8a505498a233b2542879c61
prathikkshaG <142028590+prathikkshaG@users.noreply.github.com>
2025-08-19 12:41:51 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUISPMDemo",
+    platforms: [
+        .macOS(.v13), .iOS(.v17)
+    ],
+    products: [
+        .executable(
+            name: "SwiftUISPMDemo",
+            targets: ["SwiftUISPMDemo"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "SwiftUISPMDemo",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "SwiftUISPMDemoTests",
+            dependencies: ["SwiftUISPMDemo"]
+        )
+    ]
+)
