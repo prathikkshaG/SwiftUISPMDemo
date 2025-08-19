9c764fbf3306c9b1258b7356726743c08f85ab2b
prathikkshaG <142028590+prathikkshaG@users.noreply.github.com>
2025-08-19 12:58:52 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUISPMDemo",
     platforms: [
-        .macOS(.v13), .iOS(.v17)
+        .macOS(.v13), .iOS(.v16)
     ],
     products: [
         .executable(
